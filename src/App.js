3c0bffcab596a205a5e48a789f048c2379d79f1e
Chase Sobey <christopher.chase.sobey@gmail.com>
2023-07-30 09:52:25 -0700

    Completion of Full Stack Course part 2-a

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
-import { useState } from 'react';
+import Note from './components/Notes'
 
-function History({ allClicks }) {
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        The app is used by pressing buttons.
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        button press history: {allClicks.join(', ')}
-      </div>
-    );
-  };
-};
-function Button({ handleClick, text }) {
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  );
-};
-
-function App() {
-  const [ value, setValue ] = useState(10);
-  function setToValue(newValue) {
-    return function () {
-      console.log('value now', newValue);
-      setValue(newValue);
-    }
-  }
+function App(props) {
+  const { notes } = props;
 
   return (
     <div>
-      {value}
-      <Button handleClick={setToValue(1000)} text='thousand' />
-      <Button handleClick={setToValue(0)} text='reset' />
-      <Button handleClick={setToValue(value + 1)} text='increment' />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
-
 }
 
 export default App;
